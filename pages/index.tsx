d3e67fdc5d4a403a6b1cc47d6ede44691552a1b8
MSpilari <mb.spilari@hotmail.com>
2022-11-28 12:38:22 -0300

    Adicionando WelcomeTxt, AboutUsCard e Houses

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
+import { AboutUsCard } from '../src/components/AboutUsCard'
+import { Houses } from '../src/components/Houses'
 import { Navbar } from '../src/components/Navbar'
 import { WelcomeImg } from '../src/components/WelcomeImg'
 import { WelcomeTxt } from '../src/components/WelcomeTxt'
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen overflow-x-hidden relative flex flex-col">
+    <main className="overflow-x-hidden relative flex flex-col">
       <WelcomeImg />
 
       <Navbar />
 
       <WelcomeTxt />
+
+      <AboutUsCard />
+
+      <Houses />
     </main>
   )
 }
