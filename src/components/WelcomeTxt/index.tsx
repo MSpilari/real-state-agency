c8fef87822b7f221c5b8cc4bd895236c8f860f0c
MSpilari <mb.spilari@hotmail.com>
2022-11-28 12:30:11 -0300

    Adicionando uma altura para mobile e desktop screens

--- src/components/WelcomeTxt/index.tsx
+++ src/components/WelcomeTxt/index.tsx
@@ -1,8 +1,9 @@
 import { AiOutlineArrowRight } from 'react-icons/ai'
+import { TopRightRoundedButton } from '../TopRightRoundedButton'
 
 const WelcomeTxt = () => {
   return (
-    <div className="h-screen flex flex-col justify-center gap-4 text-white px-4 md:px-32">
+    <div className="h-[calc(100vh_-_12rem)] lg:h-[calc(100vh_-_14rem)] w-full flex flex-col justify-center gap-4 text-white px-4 md:px-32">
       <h1 className="text-4xl w-[14rem] font-bold lg:text-6xl lg:w-[25rem]">
         Os melhores imóveis para você.
       </h1>
@@ -12,9 +13,9 @@ const WelcomeTxt = () => {
         segurança e comodidade.
       </h3>
 
-      <button className="w-40 h-10 flex items-center gap-1 bg-yellow-500 text-black font-bold rounded-tr-2xl px-10 py-1 lg:hidden">
+      <TopRightRoundedButton extraStyles="w-40 h-10 flex bg-yellow-500 font-bold text-black lg:hidden">
         Anuncie <AiOutlineArrowRight />
-      </button>
+      </TopRightRoundedButton>
     </div>
   )
 }
