ec9cdd059a4d874db26265c581c80d6e516739a3
MSpilari <mb.spilari@hotmail.com>
2022-11-30 12:01:09 -0300

    Inserindo novos tamanhos de tela

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
+    screens: {
+      '3xsm': '320px',
+      '2xsm': '375px',
+      xsm: '425px',
+      ...defaultTheme.screens
+    },
     extend: {}
   },
   plugins: []
