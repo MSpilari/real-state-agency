62b95e94a8c22c75803f08787c425ae9a793b0d0
MSpilari <mb.spilari@hotmail.com>
2022-11-30 12:10:27 -0300

    Criando o HouseCard e suas tipagens

---
+++ src/components/Houses/HouseCard/HouseCard.types.ts
@@ -0,0 +1,11 @@
+export type HouseCardProps = {
+  image: string
+  title: string
+  details: {
+    bedrooms: number
+    suite: number
+    bathrooms: number
+    cars: number
+    price: number
+  }
+}
