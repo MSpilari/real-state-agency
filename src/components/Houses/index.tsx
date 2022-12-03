7df11f2a03f23e642fac557876344ee6993cb2f1
MSpilari <mb.spilari@hotmail.com>
2022-12-03 13:00:46 -0300

    Usuário agora pode filtrar os imóveis

--- src/components/Houses/index.tsx
+++ src/components/Houses/index.tsx
@@ -1,10 +1,36 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, ChangeEvent } from 'react'
+import Link from 'next/link'
 import { HouseCard } from './HouseCard'
 import apiCall from '../../utils/AllProperties.json'
-import { Properties } from './Houses.types'
+import { Properties, FiltersProps } from './Houses.types'
 
 const Houses = () => {
   const [allProperties, setAllProperties] = useState([] as Properties)
+  const [filters, setFilters] = useState({} as FiltersProps)
+
+  const propertyFilter = (event: ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = event.target
+
+    setFilters(prevState => {
+      return {
+        ...prevState,
+        [name]: value
+      }
+    })
+  }
+
+  const filteredProperties = allProperties.filter(property => {
+    const { location, type } = filters
+
+    if (!location && !type) return allProperties
+
+    if (location && type)
+      return property.district === location && property.type === type
+
+    if (location && !type) return property.district === location
+
+    if (!location && type) return property.type === type
+  })
 
   useEffect(() => {
     setAllProperties(apiCall)
@@ -20,14 +46,14 @@ const Houses = () => {
 
       {/* Searching Bar */}
       <div className="w-full py-3 flex items-center justify-center gap-4">
-        <select name="location">
+        <select name="location" onChange={e => propertyFilter(e)}>
           <option value="">Localização</option>
           <option value="Pq. Das Nações">Pq. Das Nações</option>
           <option value="Vila Metalúrgica">Vila Metalúrgica</option>
           <option value="Camilópolis">Camilópolis</option>
         </select>
 
-        <select name="type">
+        <select name="type" onChange={e => propertyFilter(e)}>
           <option value="">Tipo</option>
           <option value="Casa">Casa</option>
           <option value="Sobrado">Sobrado</option>
@@ -38,16 +64,14 @@ const Houses = () => {
       {/* Houses List */}
       <div className="w-full grid grid-cols-2 lg:grid-cols-3 gap-4 items-center justify-items-center">
         {/* House Card */}
-        {allProperties.map(property => {
+
+        {filteredProperties.map(property => {
           const { id, images, details, title } = property
 
           return (
-            <HouseCard
-              key={id}
-              image={images[0]}
-              details={details}
-              title={title}
-            />
+            <Link key={id} href={`/imoveis/${id}`}>
+              <HouseCard image={images[0]} details={details} title={title} />
+            </Link>
           )
         })}
       </div>
