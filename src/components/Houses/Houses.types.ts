a770fcb556bef6ce73a9b7d6ec7d3aa48ae973fd
MSpilari <mb.spilari@hotmail.com>
2022-12-03 13:01:43 -0300

    Separando a tipagem em single e allProperties

--- src/components/Houses/Houses.types.ts
+++ src/components/Houses/Houses.types.ts
@@ -1,8 +1,12 @@
-export type Properties = {
+export type singleProperty = {
   id: number
   reference: string
   title: string
   address: string
+  city: string
+  state: string
+  district: string
+  type: string
   images: string[]
   details: {
     bedrooms: number
@@ -13,4 +17,11 @@ export type Properties = {
   }
   description: string
   features: string[]
-}[]
+}
+
+export type Properties = singleProperty[]
+
+export type FiltersProps = {
+  type: string | null
+  location: string | null
+}
