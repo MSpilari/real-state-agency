763174b51fb2b18231ecb15835fd337d215a37a1
MSpilari <mb.spilari@hotmail.com>
2022-12-06 13:03:59 -0300

    Separando os types, scrolling e inserindo o PropertyDetails

--- src/components/PropertyPics/index.tsx
+++ src/components/PropertyPics/index.tsx
@@ -1,22 +1,17 @@
 import Image from 'next/image'
-import { RefObject, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
+import { scrollingDiv } from '../../utils/scrollingDiv'
+import { PropertyDetails } from './PropertyDetails'
+import { PropertyPicsType } from './PropertyPics.types'
 
-type PropertyPicsType = {
-  images: string[]
-}
-
-const PropertyPics = ({ images }: PropertyPicsType) => {
+const PropertyPics = ({ images, details, reference }: PropertyPicsType) => {
   const [currentImage, setCurrentImage] = useState(images[0])
   const sliderDivRef = useRef<HTMLDivElement>(null)
 
-  const scroll = (ref: RefObject<HTMLDivElement>, pos: number) => {
-    if (ref.current) ref.current.scrollBy({ behavior: 'smooth', left: pos })
-  }
-
   return (
-    <section className="mt-24 lg:mt-40 w-full flex">
-      <div className="flex flex-col items-center lg:items-start gap-3 px-5 md:px-14 lg:px-20 xl:px-24 py-2">
+    <section className="mt-24 lg:mt-40 mb-2 w-full flex flex-col items-center md:flex-row md:items-start">
+      <div className="flex flex-col items-center lg:items-start gap-3 px-5 md:pl-14 lg:pl-20 xl:pl-28 xl:pr-20 py-2">
         <Image
           className="w-full h-60 rounded-tr-3xl md:w-96 lg:w-[35rem] lg:h-96 xl:w-[40rem]"
           width={10}
@@ -28,10 +23,10 @@ const PropertyPics = ({ images }: PropertyPicsType) => {
 
         <div className="w-full flex flex-col items-center gap-3 md:w-96 lg:w-[35rem] xl:w-[40rem]">
           <div className="flex gap-3 self-end text-lg">
-            <button onClick={() => scroll(sliderDivRef, -100)}>
+            <button onClick={() => scrollingDiv(sliderDivRef, -100)}>
               <FaArrowAltCircleLeft />
             </button>
-            <button onClick={() => scroll(sliderDivRef, 100)}>
+            <button onClick={() => scrollingDiv(sliderDivRef, 100)}>
               <FaArrowAltCircleRight />
             </button>
           </div>
@@ -55,25 +50,7 @@ const PropertyPics = ({ images }: PropertyPicsType) => {
         </div>
       </div>
 
-      <div className="hidden md:flex flex-col items-center gap-5 px-5 py-2 rounded-3xl shadow-2xl shadow-black text-xl w-48 h-fit mt-2 md:w-56 lg:w-60 xl:w-[26rem] ">
-        <div className="flex gap-3 items-center">
-          <Image
-            className="w-10 h-10 rounded-full"
-            src={images[0]}
-            sizes="25vw"
-            width={5}
-            height={5}
-            alt="Rose Gallego Pic"
-          />
-          <h1>Rose Gallego</h1>
-        </div>
-
-        <div className="flex flex-col gap-3">
-          <span>Reference</span>
-          <span>Phone</span>
-          <span>Veja no nosso Instagram</span>
-        </div>
-      </div>
+      <PropertyDetails details={details} reference={reference} />
     </section>
   )
 }
