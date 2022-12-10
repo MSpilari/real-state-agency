728f2a8098578ac1537e9f477943f15cf1ecd23d
MSpilari <mb.spilari@hotmail.com>
2022-12-10 12:56:03 -0300

    Inserindo a chamada para a API

--- pages/imoveis/[propId].tsx
+++ pages/imoveis/[propId].tsx
@@ -1,52 +1,60 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { Footer } from '../../src/components/Footer'
-import { singleProperty } from '../../src/components/Houses/Houses.types'
+import { singlePropertyAPICall } from '../../src/components/Houses/Houses.types'
 import { Navbar } from '../../src/components/Navbar'
 import { PropertyDesc } from '../../src/components/PropertyDesc'
 import { PropertyFeatures } from '../../src/components/PropertyFeatures'
 import { PropertyPics } from '../../src/components/PropertyPics'
 import { PropertySuggestions } from '../../src/components/PropertySuggestions'
 import { PropertyTitle } from '../../src/components/PropertyTitle'
-import apiCall from '../../src/utils/AllProperties.json'
 
 const PropertyPage = () => {
   const router = useRouter()
-  const [propertyInfo, setPropertyInfo] = useState({} as singleProperty)
+  const [propertyInfo, setPropertyInfo] = useState({} as singlePropertyAPICall)
+  const [loading, setLoading] = useState(true)
 
   const { propId } = router.query
 
   useEffect(() => {
-    setPropertyInfo(
-      apiCall.filter(property => property.id === Number(propId))[0]
-    )
+    const fetchData = async () => {
+      const res = await fetch(`http://localhost:3000/api/property/${propId}`)
+
+      const data = await res.json()
+
+      setPropertyInfo(data)
+
+      return setLoading(false)
+    }
+
+    fetchData()
   }, [propId])
 
   return (
     <main className="h-full overflow-x-hidden flex flex-col">
-      {Object.keys(propertyInfo).length === 0 ? (
+      {loading ? (
         <div>Loading</div>
       ) : (
         <>
           <PropertyTitle
-            title={propertyInfo.title}
-            price={propertyInfo.details.price}
-            address={propertyInfo.address}
+            title={propertyInfo.singleProperty.title}
+            price={propertyInfo.singleProperty.details.price}
+            address={propertyInfo.singleProperty.address}
           />
 
           <Navbar />
 
           <PropertyPics
-            images={propertyInfo.images}
-            details={propertyInfo.details}
-            reference={propertyInfo.reference}
+            images={propertyInfo.singleProperty.images}
+            details={propertyInfo.singleProperty.details}
+            reference={propertyInfo.singleProperty.reference}
           />
 
-          <PropertyDesc desc={propertyInfo.description} />
+          <PropertyDesc desc={propertyInfo.singleProperty.description} />
 
-          <PropertyFeatures features={propertyInfo.features} />
+          <PropertyFeatures features={propertyInfo.singleProperty.features} />
 
-          <PropertySuggestions />
+          <PropertySuggestions suggestions={propertyInfo.commonProperties} />
 
           <Footer />
         </>
