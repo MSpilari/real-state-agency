db86f99252f14cd97eaf52cd3f6b01aab6f30b4c
MSpilari <mb.spilari@hotmail.com>
2022-12-10 12:57:33 -0300

    Rota que retorna a propriedade, mais as semelhantes

---
+++ pages/api/property/[propId].ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { singlePropertyAPICall } from '../../../src/components/Houses/Houses.types'
+import AllProperties from '../../../src/utils/AllProperties.json'
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<singlePropertyAPICall>
+) {
+  const { propId } = req.query
+
+  const requestedProperty = AllProperties.filter(
+    property => property.id === Number(propId)
+  )[0]
+
+  const commonProperties = AllProperties.filter(
+    property =>
+      property.type === requestedProperty.type &&
+      property.id !== requestedProperty.id
+  )
+
+  return res
+    .status(200)
+    .json({ singleProperty: requestedProperty, commonProperties })
+}
