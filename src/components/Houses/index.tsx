ec44c917beee1fd25db386c533abf0ca8b4c3d9f
MSpilari <mb.spilari@hotmail.com>
2022-12-12 17:06:27 -0300

    Campos de pesquisa agora são o componente SearchBox

--- src/components/Houses/index.tsx
+++ src/components/Houses/index.tsx
@@ -1,24 +1,13 @@
-import { useState, useEffect, ChangeEvent } from 'react'
 import Link from 'next/link'
-import { HouseCard } from './HouseCard'
-import apiCall from '../../utils/AllProperties.json'
-import { Properties, FiltersProps } from './Houses.types'
+import { useEffect, useState } from 'react'
 import { TitleH1 } from '../TitleH1'
+import { HouseCard } from './HouseCard'
+import { FiltersProps, Properties } from './Houses.types'
+import { SearchBox } from './SearchBox'
 
 const Houses = () => {
   const [allProperties, setAllProperties] = useState([] as Properties)
   const [filters, setFilters] = useState({} as FiltersProps)
-
-  const propertyFilter = (event: ChangeEvent<HTMLSelectElement>) => {
-    const { name, value } = event.target
-
-    setFilters(prevState => {
-      return {
-        ...prevState,
-        [name]: value
-      }
-    })
-  }
 
   const filteredProperties = allProperties.filter(property => {
     const { location, type } = filters
@@ -50,25 +39,10 @@ const Houses = () => {
       <TitleH1 title="Encontre seu próximo local para viver." />
 
       {/* Searching Bar */}
-      <div className="w-full py-3 flex items-center justify-center gap-4">
-        <select name="location" onChange={e => propertyFilter(e)}>
-          <option value="">Localização</option>
-          <option value="Pq. Das Nações">Pq. Das Nações</option>
-          <option value="Vila Metalúrgica">Vila Metalúrgica</option>
-          <option value="Camilópolis">Camilópolis</option>
-        </select>
-
-        <select name="type" onChange={e => propertyFilter(e)}>
-          <option value="">Tipo</option>
-          <option value="Casa">Casa</option>
-          <option value="Sobrado">Sobrado</option>
-          <option value="Apartamento">Apartamento</option>
-        </select>
-      </div>
+      <SearchBox setFilters={setFilters} />
 
       {/* Houses List */}
       <div className="w-full grid grid-cols-2 lg:grid-cols-3 gap-4 items-center justify-items-center">
-        {/* House Card */}
 
         {filteredProperties.map(property => {
           const { id, images, details, title } = property
