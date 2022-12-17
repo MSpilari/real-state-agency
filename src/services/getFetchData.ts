fb9729f786e7264225cad043f8ca8dfe20eb611b
MSpilari <mb.spilari@hotmail.com>
2022-12-17 13:10:58 -0300

    Retorna JSON ou modifica um state

--- src/services/getFetchData.ts
+++ src/services/getFetchData.ts
@@ -2,13 +2,13 @@ import { Dispatch, SetStateAction } from 'react'
 
 const getFetchData = async (
   url: string,
-  stateHandler: Dispatch<SetStateAction<any>>
+  stateHandler?: Dispatch<SetStateAction<any>>
 ) => {
   const res = await fetch(url)
 
   const data = await res.json()
 
-  return stateHandler(data)
+  return stateHandler ? stateHandler(data) : data
 }
 
 export { getFetchData }
